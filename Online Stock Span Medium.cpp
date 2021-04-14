b798dcd0dcaf9aa16d14b5f0569f4a1f5dfcd0d2
Akanksha Saini <37983977+akanksha-saini@users.noreply.github.com>
2021-04-15 02:42:16 +0530

    Create Online Stock Span Medium.cpp

---
+++ Online Stock Span Medium.cpp
@@ -0,0 +1,38 @@
+/*
+https://leetcode.com/problems/online-stock-span/submissions/
+
+only 19% acceptance please optimize
+*/
+
+class StockSpanner {
+    stack<pair<int,int>> s;
+    int index;
+public:
+    
+    StockSpanner()
+    {
+        index = -1;
+    }
+    int next(int price)
+    {
+        index += 1;
+        
+        while(!s.empty() &&  s.top().second <= price)
+            s.pop();
+        
+        if(s.empty())
+        {
+            s.push({index,price}); 
+            return index + 1;
+        }
+        int res = s.top().first;
+        s.push({index,price});
+        return index - res; 
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
